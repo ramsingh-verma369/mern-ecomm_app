3953ed753905a4f6f8b09560e8d1dc26b7f7e1d0
ramsingh-verma369 <ramsinghverma9450@gmail.com>
2024-09-01 14:37:36 +0530

    "coupon route completed"

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.route.js"
 import productRoutes from "./routes/product.route.js"
 import cartRoutes from "./routes/cart.route.js"
+import couponRoutes from "./routes/coupon.route.js"
 
 import { connectDB } from "./lib/db.js";
 dotenv.config();
@@ -19,6 +20,7 @@ app.use(cookieParser());
 app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/products',productRoutes);
 app.use('/api/v1/carts',cartRoutes);
+app.use('/api/v1/coupons',couponRoutes);
 
 
 app.listen(PORT,() => {
