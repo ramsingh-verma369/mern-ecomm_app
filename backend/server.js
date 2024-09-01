98b4f561ee2b55348b24a5bc61d7f7342345ff74
ramsingh-verma369 <ramsinghverma9450@gmail.com>
2024-09-01 11:22:20 +0530

    "completed to cart controller"

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.route.js"
 import productRoutes from "./routes/product.route.js"
+import cartRoutes from "./routes/cart.route.js"
 
 import { connectDB } from "./lib/db.js";
 dotenv.config();
@@ -17,6 +18,7 @@ app.use(cookieParser());
 
 app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/products',productRoutes);
+app.use('/api/v1/carts',cartRoutes);
 
 
 app.listen(PORT,() => {
